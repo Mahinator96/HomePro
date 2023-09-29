e149ae4578b71a5ae4465fe144a3a8239a00f580
Evgeniy Vasiliev <vasilievdevelop@gmail.com>
2023-09-29 12:27:13 +0300

    create section 'how' & response

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ import './modules/isWebp.js';
 // Расскоментировать, если нужен динамический адаптив
 import './modules/dynamic-adaptiv.js';
 // Расскоментировать если нужен бургер
-import './modules/burger.js';
+// import './modules/burger.js';
 
 // Расскоментировать для плавной навигации header при клике (Структура в файле js)
 // import './modules/header-scroll-to-click.js';
